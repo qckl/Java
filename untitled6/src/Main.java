5b43c3c8e2d762aef3ba73d9bafe74e6eab12c1e
qckl <sobakogavgav@gmail.com>
2020-03-21 16:31:41 +0300

    Задачи на движение
    Доработано

--- untitled6/src/Main.java
+++ untitled6/src/Main.java
@@ -1,8 +1,9 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[] args) {
+    public static void main (String[] args) throws InterruptedException {
 
         int t;
         Scanner in = new Scanner(System.in);
@@ -12,9 +13,14 @@ public class Main {
         int v = in.nextInt();
         t=s/v;
         System.out.println("t=" + t);
+        String q = ".";
+
+
         if (s<=100) {
-            for (int i = 0; i<=400; i++){
-                System.out.print(".");
+            for (int i = 0; i<=s; i++){
+                Thread.sleep (2000);
+                System.out.print("\b");
+                System.out.print(" "+ q);
             }
             }
         else if (s>100){
